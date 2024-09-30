53c9739787e65d13d45d1687bfbde7da57b28d60
marcos.padin@outlook.es <you@example.com>
2024-09-30 11:34:11 +0200

    Tarea 29 solucionada

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,10 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            int x = 12/0;
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }
